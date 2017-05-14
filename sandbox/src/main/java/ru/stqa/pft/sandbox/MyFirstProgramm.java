0dcd8198740ddda6bbb368541c679e8979c6b54d
Arciom <swert.rem@gmail.com>
2017-05-14 12:04:32 +0300

    Добавлено неколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -1,7 +1,18 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgramm {
-public static void main (String[] args){
-System.out.println("Hello, world");
-	}
+  public static void main(String[] args) {
+  double l = 5;
+  double a = 3.5;
+  double b = 5.9;
+    System.out.println("Квадрат = " + area(l));
+    System.out.println("Прямоугольник: " + area(4.9, 1.3));
+  }
+
+  public static double area(double side) {
+    return side * side;
+  }
+  public static double area(double a, double b) {
+    return a * b;
+  }
 }
