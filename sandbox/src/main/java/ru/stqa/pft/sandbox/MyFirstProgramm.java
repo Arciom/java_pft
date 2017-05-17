03aaa202fca7228a9da03cfe9017b4cf727a8c4f
Arciom <swert.rem@gmail.com>
2017-05-17 21:48:07 +0300

    Добавили функции и тесты.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -2,17 +2,28 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgramm {
   public static void main(String[] args) {
-  double l = 5;
-  double a = 3.5;
-  double b = 5.9;
-    System.out.println("Квадрат = " + area(l));
-    System.out.println("Прямоугольник: " + area(4.9, 1.3));
-  }
-
-  public static double area(double side) {
-    return side * side;
-  }
-  public static double area(double a, double b) {
-    return a * b;
+    Square s = new Square(5);
+    Rectangle r = new Rectangle(3.5, 8.3);
+    System.out.println("Квадрат = " + s.area());
+    System.out.println("Прямоугольник: " + r.area());
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by arciom on 16.05.2017.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+
+}
