8253181303b84f08abef5f579fde8afb435e61f2
Arciom <swert.rem@gmail.com>
2017-05-21 18:14:19 +0300

    Лекция 2.7. Выделяем специализированные классы-помощники

---
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by arciom on 21.05.2017.
+ */
+public class NavigationHelper {
+  private FirefoxDriver wd;
+
+  public NavigationHelper(FirefoxDriver wd) {
+    this.wd = wd;
+  }
+
+  public void gotoGroupPage() {
+    wd.findElement(By.linkText("GROUPS")).click();
+  }
+}
